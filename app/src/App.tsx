c79813701c6171c138b8da0a537157f565ff7e1b
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-03-12 17:43:30 +0900

    feat: UserIndex を Dashbord へ埋め込み

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,13 +4,11 @@ import React from 'react'
 
 import './App.css'
 import Dashboard from './components/modules/Dashboard'
-import { UserIndex } from './components/pages/UserIndex'
 
 function App() {
   return (
     <div className="App">
-      {/* <Dashboard /> */}
-      < UserIndex />
+      <Dashboard />
     </div>
   )
 }

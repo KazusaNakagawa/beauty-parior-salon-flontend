a777a1246e2193e88a9810a83a56f92164e0d6d3
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-03-12 14:37:25 +0900

    feat: UserIndex を表示

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,18 @@
 /*eslint no-lone-blocks: "error"*/
 /*eslint-env es6*/
-import React from 'react';
+import React from 'react'
 
 import './App.css'
 import Dashboard from './components/modules/Dashboard'
+import { UserIndex } from './components/pages/UserIndex'
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      {/* <Dashboard /> */}
+      < UserIndex />
     </div>
   )
 }
 
-export default App;
+export default App

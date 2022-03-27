443f1c7f656ae3e656a3ae8444bef44faa5f1453
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-03-27 13:14:55 +0900

    feat: react-router-dom で、ページ遷移を追加

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,24 @@
 /*eslint no-lone-blocks: "error"*/
 /*eslint-env es6*/
 import React from 'react'
-
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Dashboard from './components/modules/Dashboard'
-import Registration from './components/Registration'
+import UserRegst from './components/pages/UserRegstraPage'
+import UserIndex from './components/UserIndex'
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-      <Registration />
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* <Dashboard /> */}
+        <Routes>
+          <Route path="/" element={<UserIndex />} />
+          <Route path="/user-regst" element={<UserRegst />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
-
 export default App

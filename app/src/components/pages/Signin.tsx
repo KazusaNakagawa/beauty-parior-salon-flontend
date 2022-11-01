5fd51f8a0323ea701ef2411d9259951189443b32
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-11-01 22:40:30 +0900

    feat: delete debug print

--- app/src/components/pages/Signin.tsx
+++ app/src/components/pages/Signin.tsx
@@ -58,13 +58,8 @@ export default function SignIn() {
     })
       .then(function (res) {
         alert('Success Login')
-        console.log({ data: res.data })
-        console.log({ status: res.status })
         // https://developer.mozilla.org/ja/docs/Web/API/Window/localStorage
-        const setToken = localStorage.setItem('token', res.data.access_token)
-        console.log(setToken)
-        var getToken = localStorage.getItem('token')
-        console.log(getToken)
+        localStorage.setItem('token', res.data.access_token)
         navigate('/about')
       })
       .catch(function (error) {
